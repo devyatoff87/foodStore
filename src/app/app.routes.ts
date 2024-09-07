801ff81a6521f8ecd8542add758d848be53628d7
Vladimir Devyatov <vladimir.devyatov@brandad.de>
2024-09-07 13:16:05 +0200

    app routing for food

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './core/components/mainlayout/mainlayout.component';
-import { FoodlistComponent } from './features/components/foodlist/foodlist.component';
 
 export const routes: Routes = [
   {
-    path: '', component: MainLayoutComponent, children: [
+    path: '',
+    component: MainLayoutComponent,
+    children: [
       {
         path: '',
-        loadChildren: () =>
-          import('./features/components/food.routes')
-      }
-    ]
-  }
+        loadChildren: () => import('./features/components/food.routes'),
+      },
+    ],
+  },
 ];
--- src/app/features/components/food.routes.ts
+++ src/app/features/components/food.routes.ts
@@ -1,4 +1,19 @@
 import { Routes } from '@angular/router';
 import { FoodlistComponent } from './foodlist/foodlist.component';
+import { FoodDetailsComponent } from './food-details/food-details.component';
 
-export default [{ path: '', component: FoodlistComponent }] as Routes;
+export default [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: FoodlistComponent,
+      },
+      {
+        path: 'food-details/:id',
+        component: FoodDetailsComponent,
+      },
+    ],
+  },
+] as Routes;

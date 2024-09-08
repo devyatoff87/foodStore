3f0573a62189eb2e313d2d6f315ecc2682445a88
Vladimir Devyatov <v.devyatov@gmail.com>
2024-09-08 15:28:54 +0200

    style: heading and button styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,13 @@ module.exports = {
         '60': '60%',
         '70': '70%',
         '80': '80%',
-
       },
+      fontSize: {
+        'heading-1': '40px',
+        'heading-2': '32px',
+        'heading-3': '24px',
+        'heading-4': '20px',
+      }
     },
   },
   plugins: [],

04aecd1e371e38f6c5e8776d31a804e71ba1a638
Vladimir Devyatov <v.devyatov@gmail.com>
2024-09-08 12:45:37 +0200

    fix: food router adjustments

--- src/app/features/components/food.routes.ts
+++ src/app/features/components/food.routes.ts
@@ -1,15 +1,21 @@
 import { Routes } from '@angular/router';
 import { FoodlistComponent } from './foodlist/foodlist.component';
 import { FoodDetailsComponent } from './food-details/food-details.component';
+import { FoodComponent } from './food/food.component';
 
 export default [
   {
     path: '',
-    component: FoodlistComponent,
+    component: FoodComponent,
+    children: [
+      {
+        path: '',
+        component: FoodlistComponent,
+      },
+      {
+        path: 'food/:id',
+        component: FoodDetailsComponent,
+      }
+    ]
   },
-  {
-    path: 'food/:id',
-    component: FoodDetailsComponent,
-  }
-
 ] as Routes;
--- src/app/features/components/food/food.component.ts
+++ src/app/features/components/food/food.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "../../../core/components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-food',
   standalone: true,
-  imports: [RouterOutlet],
-  template: '<router-outlet > </router-outlet>'
+  imports: [RouterOutlet, SidebarComponent],
+  templateUrl: './food.component.html'
 
 })
 export class FoodComponent { }
